70093cf01c531da8942708c99166eef7bf266184
Manish Vishwakarma <hbxmanish@gmail.com>
2023-08-28 19:29:38 +0530

    About Section done

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -13,7 +13,7 @@ export default function About() {
   const [showSecondParagraph, setShowSecondParagraph] = useState(false);
 
   const toggleSecondParagraph = () => {
-    setShowSecondParagraph(!showSecondParagraph)
+    setShowSecondParagraph(!showSecondParagraph);
   };
 
   return (
@@ -27,34 +27,30 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-          Im a developer with 3 years of experience...
+        <span className="font-bold">I am a Developer, Freelancer, Sketch Artist & A Gamer</span> <br />
+        In the summer of 21, I started programming. & I started with ...
       </p>
       {showSecondParagraph ? (
         <motion.p
-          initial={{ opacity: 0, y: 20}}
-          animate={{ opacity: 1, y:0 }}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.9 }}
         >
-        <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
+          <p>
+            <span className="font-bold">Python</span> later than most, I dived into the Development stuff and still in there. Now talking about <span className="font-bold">"What I have done so far in this field ?" </span> - I have a 3.5 star rating in HackerRank for Problem solving , Contributor to Cal.com & GitHub CLi , Blogger at Hashnode(Current) , Worked on Hackathon Projects.
+          </p>
         </motion.p>
-      ): null}
+      ) : null}
       <Link
-          href="/"
-          onClick={(e) => {
-            e.preventDefault();
-            toggleSecondParagraph();
-          }}
-          className="group justify-center bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-        >
-          {showSecondParagraph ? "Read less" : "Read more"}{" "}
-        </Link>
+        href="/"
+        onClick={(e) => {
+          e.preventDefault();
+          toggleSecondParagraph();
+        }}
+        className="group justify-center bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+      >
+        {showSecondParagraph ? "Read less" : "Read more"}{" "}
+      </Link>
     </motion.section>
   );
 }
